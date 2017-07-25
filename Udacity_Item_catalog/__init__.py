0938d0dd63fdcc4f26012889362cd5c542dd49b2
Zac <zmrowicki@hotmail.com>
2017-07-25 16:06:54 -0700

    Straighten out the db uri and test it out with the interpreter

--- Udacity_Item_catalog/__init__.py
+++ Udacity_Item_catalog/__init__.py
@@ -1,8 +1,12 @@
 import os
+import sqlite3
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# _basedir = os.path.abspath(os.path.dirname(__file__))
+#DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'flask-website.db'))'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'catalog.db')
 
 db = SQLAlchemy(app)
 
--- Udacity_Item_catalog/models.py
+++ Udacity_Item_catalog/models.py
@@ -1,11 +1,14 @@
+import datetime
 from Udacity_Item_catalog import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
+    created_at = db.Column(db.DateTime, nullable=False)
 
     def __init__(self, username, email):
         self.username = username
         self.email = email
+        self.created_at = datetime.datetime.utcnow()
 

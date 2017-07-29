4468a722731d2a2eff20192c2d69273489489e36
Zac <zmrowicki@hotmail.com>
2017-07-28 18:24:34 -0700

    Add config file and appropriate import into __init__

--- Udacity_Item_catalog/__init__.py
+++ Udacity_Item_catalog/__init__.py
@@ -4,9 +4,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# _basedir = os.path.abspath(os.path.dirname(__file__))
-#DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'flask-website.db'))'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'catalog.db')
+app.config.from_object('config.DevelopmentConfig')
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'catalog.db')
 
 db = SQLAlchemy(app)
 
